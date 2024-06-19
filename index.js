8d4bacf96812bf1fa55043248e5a5d33c7b53c5c
KatyaPC <svyatoslav.stolyarov.s.sss2000@mail.ru>
2024-06-19 18:39:42 +0300

    стилизовал header-info не сделав треугольную форму, есть ошибка с цветом кнопки

---
+++ index.js
@@ -0,0 +1,15 @@
+import './scss/style.scss';
+
+import './images/document_icon.svg';
+import './images/dollar_icon.svg';
+import './images/instagram_footer.svg';
+import './images/logo.svg';
+import './images/phone_footer.svg';
+import './images/scales_icon.svg';
+import './images/telegram_footer.svg';
+import './images/telegram_logo.svg';
+import './images/vk_footer.svg';
+import './images/whatsapp_footer.svg';
+import './images/whatsapp_logo.svg';
+import './images/bgheader.jpg';
+import './images/bgmanheader.png';
